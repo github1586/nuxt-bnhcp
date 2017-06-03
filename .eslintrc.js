9fe1256b688e89684be0e67a9fe8d44d56ea3843
bobo <weibo392@163.com>
2017-06-03 12:31:20 +0800

    del

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    // "env": {
+    //     "browser": true
+    // },
+    // "extends": "eslint:recommended",
+    // "rules": {
+    //     "indent": [
+    //         "error",
+    //         "tab"
+    //     ],
+    //     "linebreak-style": [
+    //         "error",
+    //         "unix"
+    //     ],
+    //     "quotes": [
+    //         "error",
+    //         "double"
+    //     ],
+    //     "semi": [
+    //         "error",
+    //         "always"
+    //     ]
+    // }
+};
