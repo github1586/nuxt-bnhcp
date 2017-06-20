ef592cdf3aeb8d8ab81a70105817f23cdb95533e
guanbo <guanbobo0@gmail.com>
2017-06-20 14:43:38 +0800

    1

---
+++ store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = () => new Vuex.Store({
+
+  state: {
+    counter: 0
+  },
+  mutations: {
+    increment (state) {
+      state.counter++
+    }
+  }
+})
+
+export default store
