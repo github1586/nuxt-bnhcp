8f9f719d73f15eb59df0dd309679b65a63035f64
guanbobo <guanbobo0@gmail.com>
2017-08-20 12:55:58 +0800

    gif

--- components/home/RecommendedSchools.vue
+++ components/home/RecommendedSchools.vue
@@ -5,27 +5,27 @@
 			<ul class="cls_list">
 				<li class="fl">
 					<a href="123" class="con" id="dao_acti1">
-						<img src="img/tui_school.png" alt="" />
+						<img src="img/school_1.jpg" alt="" />
 					</a>
-					<p>杰睿学校(海..</p>
+					<p>中考冲刺</p>
 				</li>
 				<li class="">
 					<a href="javascript:;" class="con" id="dao_acti2">
-						<img src="img/tui_school.png" alt="" />
+						<img src="img/school_2.png" alt="" />
 					</a>
-					<p>杰睿学校(海..</p>
+					<p>新东方教育</p>
 				</li>
 				<li class="">
 					<a href="123" class="con" id="dao_acti1">
-						<img src="img/tui_school.png" alt="" />
+						<img src="img/school_3.jpg" alt="" />
 					</a>
-					<p>杰睿学校(海..</p>
+					<p>英语四六级</p>
 				</li>
 				<li class="">
 					<a href="javascript:;" class="con" id="dao_acti2">
-						<img src="img/tui_school.png" alt="" />
+						<img src="img/school_4.jpg" alt="" />
 					</a>
-					<p>杰睿学校(海..</p>
+					<p>3节课</p>
 				</li>
 			</ul>
 		</div>           
